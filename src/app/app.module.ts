c75bc32e72df8c6b80eb987bb56e26ba11eefbab
ArminCicak <armincicak@gmailcom>
2021-01-26 19:55:50 +0100

    Bilo šta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { UnesiZaposlenogComponent } from './unesi-zaposlenog/unesi-zaposlenog.component';
 import { NovaTransakcijaComponent } from './nova-transakcija/nova-transakcija.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
